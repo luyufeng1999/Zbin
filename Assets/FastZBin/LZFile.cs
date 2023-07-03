a7716048682b1c6ed08a7eb62963841169cfe216
nzcv <95273210+nzcv@users.noreply.github.com>
2023-07-03 11:49:54 +0800

    add readme

--- Assets/FastZBin/LZFile.cs
+++ Assets/FastZBin/LZFile.cs
@@ -1,7 +1,28 @@
-﻿namespace FastZBin
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FastZBin
 {
     public class LZFile
     {
-        
+            #if UNITY_IOS && !UNITY_EDITOR
+	        const string TRACK_DLL = "__Internal";
+            #else
+            const string TRACK_DLL = "zfile";
+            #endif
+            [DllImport(TRACK_DLL, EntryPoint = "LZOpen")]
+            public static extern IntPtr Open(string zFilePath, string mode);
+    
+            [DllImport(TRACK_DLL, EntryPoint = "LZLoadHeader")]
+            public static extern IntPtr LoadHeader(IntPtr zFileHandle);
+    
+            [DllImport(TRACK_DLL, EntryPoint = "LZGetBlockSize")]
+            public static extern UInt64 GetBlockSize(IntPtr zFileHandle, int key, int key2);
+    
+            [DllImport(TRACK_DLL, EntryPoint = "LZGetBlock")]
+            public static extern UInt64 Get(IntPtr zFileHandle, int key, int key2, byte[] buffer, int size);
+    
+            [DllImport(TRACK_DLL, EntryPoint = "LZClose")]
+            public static extern int Close(IntPtr zFileHandle);
     }
 }
